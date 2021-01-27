82dd5193a978cb42df879412dcf203bab1757923
disharjayantha <dishuj15@gmail.com>
2021-01-27 16:16:59 +0530

    added done channel to prev example

--- 05-context/02-concpatterns/main.go
+++ 05-context/02-concpatterns/main.go
@@ -5,35 +5,45 @@ import (
 	"sync"
 )
 
-func gen(nums ...int) <-chan int {
-	out := make(chan int)
-	go func() {
-		for _, n := range nums {
-			out <- n
+func gen(done <-chan struct{}, nums ...int) <-chan int {
+	out := make(chan int, len(nums))
+	for _, n := range nums {
+		select {
+		case out <- n:
+		case <-done:
+			return out
 		}
-		close(out)
-	}()
+	}
+	close(out)
 	return out
 }
 
-func sq(in <-chan int) <-chan int {
+func sq(done <-chan struct{}, in <-chan int) <-chan int {
 	out := make(chan int)
 	go func() {
 		for num := range in {
-			out <- num * num
+			select {
+			case out <- num * num:
+			case <-done:
+				return
+			}
 		}
 		close(out)
 	}()
 	return out
 }
 
-func merge(cs ...<-chan int) <-chan int {
+func merge(done <-chan struct{}, cs ...<-chan int) <-chan int {
 	var wg sync.WaitGroup
 	out := make(chan int)
 
 	output := func(c <-chan int) {
 		for num := range c {
-			out <- num
+			select {
+			case out <- num:
+			case <-done:
+				return
+			}
 		}
 		wg.Done()
 	}
@@ -52,8 +62,12 @@ func merge(cs ...<-chan int) <-chan int {
 }
 
 func main() {
-	num := gen(1, 2, 3, 4, 5)
-	out := sq(num)
+	done := make(chan struct{})
+	defer close(done)
+
+	num := gen(done, 1, 2, 3, 4, 5)
+	out := sq(done, num)
+
 	for {
 		sqred, ok := <-out
 		if !ok {
@@ -63,21 +77,33 @@ func main() {
 		fmt.Println(sqred)
 	}
 
-	for n := range sq(sq(gen(2, 4))) {
+	for n := range sq(done, sq(done, gen(done, 2, 4))) {
 		fmt.Println(n)
 	}
 
 	fmt.Println("Using fan out and fan in")
 
-	in := gen(2, 3, 4, 5, 6, 7, 8)
+	in := gen(done, 2, 3, 4, 5, 6, 7, 8)
 
-	c1 := sq(in)
-	c2 := sq(in)
+	c1 := sq(done, in)
+	c2 := sq(done, in)
 
-	numChan := merge(c1, c2)
+	numChan := merge(done, c1, c2)
 	for num := range numChan {
 		fmt.Println(num)
 	}
+
+	// Incase you decided to accept only 1, 2 or few values from merge func then
+	// you can do it and it wont hang any goroutine , since defer done will be
+	// called at the end that will trigger all goroutine to be returned.
+	// Ex:
+	// numChan := merge(done, c1, c2)
+	// fmt.Println(<-numChan)
+	// only 1 value gets accepted in above case.
+	// Belore other values even gets started or already in process, when done gets
+	// called it all goroutine will exit.
+	// if u dont use done pattern then goroutine will not be exited and it will take
+	// some resourses and also goroutines are not garbaged collected.
 
 	fmt.Println("Exited")
 }
