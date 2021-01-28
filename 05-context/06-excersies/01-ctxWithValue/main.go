a9c3d71f091ea5a0f96081ba142bea6f435f347c
disharjayantha <dishuj15@gmail.com>
2021-01-28 11:47:27 +0530

    context withValue example

---
+++ 05-context/06-excersies/01-ctxWithValue/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/disharjayanth/go-explore-interface-error-cxt-arch/05-context/06-excersies/01-ctxWithValue/user"
+)
+
+func main() {
+	ctx := context.Background()
+	ctx = user.AddUserID(ctx, 1)
+	ctx = user.AddUserName(ctx, "James Bond")
+
+	if id := (user.GetUserID(ctx)); id != nil {
+		fmt.Println("User id from context: ", *id)
+	} else {
+		fmt.Println("User id not found")
+	}
+
+	if name := user.GetUserName(ctx); name != nil {
+		fmt.Println("User name from context: ", *name)
+	} else {
+		fmt.Println("User name not found")
+	}
+}
---
+++ 05-context/06-excersies/01-ctxWithValue/user/user.go
@@ -0,0 +1,39 @@
+package user
+
+import (
+	"context"
+)
+
+type key int
+
+const userID key = 0
+const userName key = 1
+const userName2 key = 2
+
+// AddUserID adds in a userid for given ctx
+func AddUserID(ctx context.Context, id int) context.Context {
+	return context.WithValue(ctx, userID, id)
+}
+
+// AddUserName adds in a username for given ctx
+func AddUserName(ctx context.Context, name string) context.Context {
+	return context.WithValue(ctx, userName, name)
+}
+
+// GetUserID gets id for given ctx
+func GetUserID(ctx context.Context) *int {
+	id, ok := ctx.Value(userID).(int)
+	if !ok {
+		return nil
+	}
+	return &id
+}
+
+// GetUserName gets name for given ctx
+func GetUserName(ctx context.Context) *string {
+	name, ok := ctx.Value(userName2).(string)
+	if !ok {
+		return nil
+	}
+	return &name
+}
